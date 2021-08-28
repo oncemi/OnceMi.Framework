54eb0a5f6763a4a8f887e0f48f73d98d577d7fe5
withsalt <sysrun@163.com>
2021-08-28 22:43:13 +0800

    优化sqlite arm安装脚本

--- docs/sqlite_arm_install.sh
+++ docs/sqlite_arm_install.sh
@@ -6,9 +6,17 @@ if [ ! -f "sqlite_arm.zip" ];then
   echo "Download file failed."
   exit 1
 fi
-sudo apt update
-sudo apt install unzip
+#install unzip
+if unzip -h > /dev/null 2>&1; then
+    echo "unzip exist, skip install unzip."
+else
+    echo "unzip not exist, install it!"
+	sudo apt update
+	sudo apt install unzip -y
+fi
+#delete old
 sudo rm -rf libSQLite.Interop.so SQLite.Interop.dll
 unzip sqlite_arm.zip
 sudo mv sqlite_arm/SQLite.Interop.dll sqlite_arm/libSQLite.Interop.so ./
 sudo rm -rf sqlite_arm* sqlite_arm_install.sh
+echo "Installation is complete."
